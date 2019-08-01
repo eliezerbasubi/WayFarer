e70aee69e5f428149a851793ce68daef02047951
Eliezer <eliezer.basubi30@gmail.com>
2019-08-01 12:40:10 +0200

    ch(dev): add status codes variables add feedback messages add response messages

---
+++ server/constants/responseCodes.js
@@ -0,0 +1,9 @@
+export const SUCCESS_CODE = 200;
+export const CREATED_CODE = 201;
+export const NOT_FOUND_CODE = 404;
+export const BAD_REQUEST_CODE = 400;
+export const INTERNAL_SERVER_ERROR_CODE = 500;
+export const FORBIDDEN_CODE = 403;
+export const UNAUTHORIZED_CODE = 401;
+export const RESOURCE_CONFLICT = 409;
+export const UNPROCESSABLE_ENTITY = 422;
---
+++ server/constants/responseMessages.js
@@ -0,0 +1,6 @@
+export const FAIL_MSG = 'Request Failed';
+export const BAD_REQUEST_MSG = 'Bad request';
+export const NOT_FOUND = 'Request Not Found';
+export const INTERNAL_SERVER_ERROR_MSG = 'Something went wrong';
+export const FORBIDDEN_MSG = 'Token Forbidden';
+export const UNAUTHORIZED_ACCESS = 'Unauthorized Access';
