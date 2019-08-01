1a165e3ded45d8b1cd9625f9bf479a5909246287
Eliezer <eliezer.basubi30@gmail.com>
2019-08-01 17:59:17 +0200

    ft(Signup): add user signup handle errors  [Finishes #167641380]

---
+++ server/controllers/userController.js
@@ -0,0 +1,70 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import omit from 'object.omit';
+import {
+  EMAIL_ALREADY_EXIST
+} from '../constants/feedback';
+import {
+  userTable,
+  User
+} from '../models/user';
+import {
+  RESOURCE_CONFLICT,
+  CREATED_CODE,
+  INTERNAL_SERVER_ERROR_CODE
+} from '../constants/responseCodes';
+import Helper from '../helpers/helper';
+
+dotenv.config();
+
+export default class UserController {
+  /**
+   * This function handles user signup data and hashes the password.
+   * For security reasons we assign user token to be expired in 1 hour.
+   * We use email, id and user status(admin or user) to build a token
+   * @param {*} request
+   * @param {*} response
+   * @param {*} next
+   */
+  static async signUp(req, res) {
+    const {
+      firstName, lastName, email, phoneNumber, password, country, city, isAdmin
+    } = req.body;
+    const details = userTable.find(user => user.email === email);
+    if (details) { return Helper.error(res, RESOURCE_CONFLICT, EMAIL_ALREADY_EXIST); }
+
+    try {
+      const saltedPassword = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(password, saltedPassword);
+
+      const user = [];
+      const tokenId = jwt.sign({
+        email,
+        id: userTable.length + 1,
+        isAdmin
+      },
+      process.env.JWT_KEY, {
+        expiresIn: '24h'
+      });
+
+      user.push(new User({
+        token: tokenId,
+        id: userTable.length + 1,
+        firstName,
+        lastName,
+        email,
+        password: hashedPass,
+        phoneNumber,
+        country,
+        city,
+        isAdmin
+      }));
+      userTable.push(...user);
+
+      return Helper.success(res, CREATED_CODE, omit(req.body, ['password', 'isAdmin']), 'Account successfully created');
+    } catch (error) {
+      return Helper.error(res, INTERNAL_SERVER_ERROR_CODE, error);
+    }
+  }
+}
