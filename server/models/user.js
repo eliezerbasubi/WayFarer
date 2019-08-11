00c8bfafc71e702ab838df8347638cf2cbd9a842
Eliezer <eliezer.basubi30@gmail.com>
2019-08-11 21:27:53 +0200

    ft(db-signup): add code refactor

--- server/models/user.js
+++ server/models/user.js
@@ -7,22 +7,12 @@ export default class UserQuery {
     try {
       const def = await creator.usersTable();
       if (def.error) {
-        return {
-          error: {
-            status: 500,
-            message: def.res
-          }
-        };
+        return { error: { status: 500, message: def.res } };
       }
 
       const exist = await pool.query('SELECT * FROM users WHERE email = $1', [values[2]]);
       if (exist.rowCount > 0) {
-        return {
-          error: {
-            status: RESOURCE_CONFLICT,
-            message: EMAIL_ALREADY_EXIST
-          }
-        };
+        return { error: { status: RESOURCE_CONFLICT, message: EMAIL_ALREADY_EXIST } };
       }
 
       const result = await pool.query(`INSERT INTO users (firstname, lastname, email,password,phone,country,city
@@ -30,13 +20,7 @@ export default class UserQuery {
 
       return result;
     } catch (e) {
-      return {
-        error: {
-          status: 500,
-          message: e,
-          err: e
-        }
-      };
+      return { error: { status: 500, message: e, err: e } };
     }
   }
 }
