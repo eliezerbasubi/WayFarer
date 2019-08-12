64558b5312146235df206375aa5445004105a326
Eliezer <eliezer.basubi30@gmail.com>
2019-08-12 08:59:35 +0200

    ch(db-test): add tests to reset password endpoint [#167829453]

--- server/tests/test.spec.js
+++ server/tests/test.spec.js
@@ -42,7 +42,6 @@ describe('Test case: User authentication Endpoint => /api/v1/auth/', () => {
   
     describe('Base case: User creates new account', () => {
         it('Should return 201. Account was successfully created', (done) => {
-            preSave.is_admin = false;
             request(app)
                 .post(routes.signup)
                 .send(preSave)
@@ -144,65 +143,62 @@ describe('Test case: User authentication Endpoint => /api/v1/auth/', () => {
         });
     });
 
-    // describe('Reset password', ()=> {
-    //     it('Should change user password',(done)=>{
-    //         request(app)
-    //         .patch('/api/v1/auth/reset/1')
-    //         .send(changePassword).end((err,res) =>{
-    //             expect(res.status).to.equal(SUCCESS_CODE);
-    //             expect(res.body).to.have.property('status').equal(SUCCESS_CODE);
-    //             expect(res.body.data).to.be.an('object');
-    //             expect(res.body.message).to.be.a("string")
-    //             done();
-    //         });
-    //     });
+    describe('Reset password', ()=> {
+        it('Should change user password',(done)=>{
+            request(app)
+            .patch('/api/v1/auth/reset/1')
+            .send(changePassword).end((err,res) =>{
+                expect(res.status).to.equal(SUCCESS_CODE);
+                expect(res.body).to.have.property('status').equal(SUCCESS_CODE);
+                expect(res.body.message).to.be.a("string")
+                done();
+            });
+        });
 
-    //     it('Should not reset password if it does not match old password',(done)=>{
-    //         changePassword.old_password = "1234567";
-    //         request(app)
-    //         .patch('/api/v1/auth/reset/1')
-    //         .send(changePassword).end((err,res) =>{
-    //             expect(res.status).to.equal(UNAUTHORIZED_CODE);
-    //             expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-    //             expect(res.body.error).to.be.equal(OLD_PASSWORD_NOT_MATCH);
-    //             done();
-    //         });
-    //     });
-    //     it('Should return 401 if confirm password does not match new password',(done)=>{
-    //         changePassword.old_password = "123456";
-    //         changePassword.confirm_password = "1234567b";
-    //         request(app)
-    //         .patch('/api/v1/auth/reset/1')
-    //         .send(changePassword).end((err,res) =>{
-    //             expect(res.status).to.equal(UNAUTHORIZED_CODE);
-    //             expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-    //             expect(res.body.error).to.be.equal(PASSWORD_DOESNT_MATCH);
-    //             done();
-    //         });
-    //     });
-    //     it('Should not reset password if ID is not found',(done)=>{
-    //         userTable.map(user => { user.id = 2 })
-    //         request(app)
-    //         .patch('/api/v1/auth/reset/1')
-    //         .send(changePassword).end((err,res) =>{
-    //             expect(res.status).to.equal(UNAUTHORIZED_CODE);
-    //             expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-    //             expect(res.body.error).to.be.equal(USER_ID_NOT_FOUND);
-    //             done();
-    //         });
-    //     });
-    //     it('Should not reset password If new password is less than 6',(done)=>{
-    //         userTable.map(user => { user.id = 1 });
-    //         changePassword.new_password = "123";
-    //         request(app)
-    //         .patch('/api/v1/auth/reset/1')
-    //         .send(changePassword).end((err,res) =>{
-    //             expect(res.status).to.equal(UNPROCESSABLE_ENTITY);
-    //             expect(res.body).to.have.property('status').equal(UNPROCESSABLE_ENTITY);
-    //             done();
-    //         });
-    //     });
-    // });
+        it('Should not reset password if it does not match old password',(done)=>{
+            changePassword.old_password = "1234567";
+            request(app)
+            .patch('/api/v1/auth/reset/1')
+            .send(changePassword).end((err,res) =>{
+                expect(res.status).to.equal(UNAUTHORIZED_CODE);
+                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
+                expect(res.body.error).to.be.equal(OLD_PASSWORD_NOT_MATCH);
+                done();
+            });
+        });
+        it('Should return 401 if confirm password does not match new password',(done)=>{
+            changePassword.old_password = "12345678";
+            changePassword.confirm_password = "1234567b";
+            request(app)
+            .patch('/api/v1/auth/reset/1')
+            .send(changePassword).end((err,res) =>{
+                expect(res.status).to.equal(UNAUTHORIZED_CODE);
+                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
+                expect(res.body.error).to.be.equal(PASSWORD_DOESNT_MATCH);
+                done();
+            });
+        });
+        it('Should not reset password if ID is not found',(done)=>{
+            request(app)
+            .patch('/api/v1/auth/reset/2')
+            .send(changePassword).end((err,res) =>{
+                expect(res.status).to.equal(UNAUTHORIZED_CODE);
+                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
+                expect(res.body.error).to.be.equal(USER_ID_NOT_FOUND);
+                done();
+            });
+        });
+        it('Should not reset password If new password is less than 6',(done)=>{
+            changePassword.new_password = "123";
+            request(app)
+            .patch('/api/v1/auth/reset/1')
+            .send(changePassword).end((err,res) =>{
+                expect(res.status).to.equal(UNPROCESSABLE_ENTITY);
+                expect(res.body).to.have.property('status').equal(UNPROCESSABLE_ENTITY);
+                done();
+            });
+        });
+    });
 
     // describe('Admin can view all users',()=>{
     //     it('Should return 404. Users not found',(done) => {
