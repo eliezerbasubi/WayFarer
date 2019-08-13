53f08a0ad158a8f355bde1da22acac9e05fc6e15
Eliezer <eliezer.basubi30@gmail.com>
2019-08-13 10:42:31 +0200

    ch(db-setup): add create all  tableson runtime

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,30 @@ import DB_URL from '../config/config';
 
 const pool = new Pool({ connectionString: DB_URL });
 
+const usersQuery = 'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(250) NOT NULL, lastname VARCHAR(250), email VARCHAR(250) NOT NULL UNIQUE,password VARCHAR(250) NOT NULL, phone VARCHAR(15) NOT NULL, country VARCHAR(50), city VARCHAR(50) NOT NULL, isAdmin BOOLEAN DEFAULT false);';
+
+const tripQuery = `CREATE TABLE IF NOT EXISTS trips(
+    id SERIAL PRIMARY KEY, 
+    trip_name VARCHAR(128), 
+    seating_capacity INTEGER NOT NULL,
+    bus_license_number VARCHAR(50),
+    origin character varying NOT NULL,
+    destination character varying NOT NULL,
+    trip_date timestamp without time zone DEFAULT NOW(),
+    arrival_date timestamp without time zone DEFAULT NOW(),
+    time TIME,
+    fare double precision,
+    status character varying DEFAULT 'active');`;
+
+const bookingQuery = `CREATE TABLE IF NOT EXISTS bookings(
+    id SERIAL,
+    trip_id INTEGER NOT NULL REFERENCES trips(id) ON DELETE RESTRICT,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
+    seat_number INTEGER NOT NULL,
+    created_on timestamp without time zone DEFAULT NOW(),
+    PRIMARY KEY(trip_id, user_id)
+);`;
+
 const dropIntest = {
   dropUserTable: async (sql) => {
     try {
@@ -14,6 +38,14 @@ const dropIntest = {
         res: 'Unable to drop table'
       };
     }
+  },
+
+  truncateTable: async (sql) => {
+    try {
+      return pool.query(sql);
+    } catch (error) {
+      throw error;
+    }
   }
 };
 
@@ -21,8 +53,7 @@ const dropIntest = {
 const creator = {
   createTable: async (query) => {
     try {
-      const res = await pool.query(query);
-      return res;
+      return pool.query(query);
     } catch (e) {
       return {
         error: true,
@@ -32,5 +63,11 @@ const creator = {
   }
 };
 
+const runner = async () => {
+  await creator.createTable(usersQuery);
+  await creator.createTable(tripQuery);
+  await creator.createTable(bookingQuery);
+};
 
-export { pool, creator, dropIntest };
+
+export { pool, runner, dropIntest };
