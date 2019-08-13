e338cbeb5ed7d060a6de861bcdd0ef919729264c
Eliezer <eliezer.basubi30@gmail.com>
2019-08-13 15:49:46 +0200

    ch(dev-test): fix merge conflict

--- server/tests/test.spec.js
+++ server/tests/test.spec.js
@@ -18,8 +18,7 @@ import {
     routes
 } from '../data/data';
 import { EMAIL_ALREADY_EXIST, OLD_PASSWORD_NOT_MATCH, PASSWORD_DOESNT_MATCH, USER_ID_NOT_FOUND, INCORRECT_PASSWORD } from '../constants/feedback';
-import { UNAUTHORIZED_ACCESS, NOT_FOUND } from '../constants/responseMessages';
-import { userTable } from '../models/user';
+import { dropIntest } from '../models';
 chai.use(chaiHttp);
 const {
     expect,
@@ -32,23 +31,30 @@ export const adminTokenId = jwt.sign({ email: preSave.email, id: 1, is_admin: pr
 export const userTokenId = jwt.sign({ email: "user@gmail.com", id: 1, is_admin: false },
     process.env.JWT_KEY, { expiresIn: '10min' });
 
-describe('Test case: User authentication Endpoint => /api/v2/auth/', () => {
+before(()=>{
+    dropIntest.dropUserTable('DROP TABLE IF EXISTS users');
+});
 
-    describe('Base case: User creates an account', () => {
-        it('Should return status 200. Correct credentials', (done) => {
+describe('Test case: User authentication Endpoint => /api/v2/auth/', () => {
+  
+    describe('Base case: User creates new account', () => {
+        it('Should return 201. Account was successfully created', (done) => {
+            preSave.is_admin = false;
             request(app)
                 .post(routes.signup)
                 .send(preSave)
+                .set('Accept', JSON_TYPE)
                 .end((err, res) => {
                     expect(res).to.have.status(CREATED_CODE);
                     expect(res.type).to.be.equal(JSON_TYPE);
                     expect(res.body).to.have.property('message');
                     expect(res.body).to.be.an('object');
+                    
                     done();
                 });
         });
 
-        it('Should return status 400. If required fields are not inserted', (done) => {
+        it('Should return 422. If required fields are not inserted', (done) => {
             request(app)
                 .post(routes.signup)
                 .send(faker)
@@ -58,7 +64,7 @@ describe('Test case: User authentication Endpoint => /api/v2/auth/', () => {
                 });
         });
 
-        it('Should not duplicate users if email already exists', (done) => {
+        it('Should return 409. User email already exists', (done) => {
             request(app)
                 .post(routes.signup)
                 .send(preSave)
@@ -70,7 +76,7 @@ describe('Test case: User authentication Endpoint => /api/v2/auth/', () => {
                 });
         });
 
-        it('Should reject explicit values. Return status 400', (done) => {
+        it('Should return 422. Reject explicit values.', (done) => {
             request(app)
                 .post(routes.signup)
                 .send(explicitData)
@@ -82,141 +88,4 @@ describe('Test case: User authentication Endpoint => /api/v2/auth/', () => {
                 });
         });
     });
-
-    describe('Base case: User logs in -> /api/v2/auth/signin', () => {
-        it('Should return status 200. Signin user with correct credentials', (done) => {
-            request(app)
-                .post(routes.signin)
-                .send(preSaveLog)
-                .end((err, res) => {
-                    expect(res.statusCode).to.be.equal(SUCCESS_CODE);
-                    expect(res.body).to.be.an('object');
-                    expect(res.body).to.have.property('status');
-                    expect(res.type).to.be.equal(JSON_TYPE);
-                    done();
-                });
-        });
-
-        it('Should return status 401. User puts fake credentials', (done) => {
-            request(app)
-                .post(routes.signin)
-                .send(fakeUser)
-                .end((err, res) => {
-                    expect(res.statusCode).to.be.equal(UNAUTHORIZED_CODE);
-                    expect(res.body.error).to.be.equal(UNAUTHORIZED_ACCESS);
-                    expect(res.body).to.have.property('status').to.be.equal(UNAUTHORIZED_CODE);
-                    done();
-                });
-        });
-
-        it('Should return status 401. Incorrect credentials', (done) => {
-            request(app)
-                .post(routes.signin)
-                .send(explicitData)
-                .end((err, res) => {
-                    expect(res.status).to.equal(UNAUTHORIZED_CODE);
-                    done();
-                });
-        });
-
-        it('Should return status 401. Incorrect password', (done) => {
-            preSaveLog.password = '123';
-            request(app)
-                .post(routes.signin)
-                .send(preSaveLog)
-                .end((err, res) => {
-                    expect(res.status).to.equal(UNAUTHORIZED_CODE);
-                    expect(res.type).to.be.equal(JSON_TYPE);
-                    expect(res.body).to.be.an('object');
-                    expect(res.body.error).to.be.equal(INCORRECT_PASSWORD)
-                    expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE)
-                    done();
-                });
-        });
-    });
-
-    describe('Reset password', ()=> {
-        it('Should change user password',(done)=>{
-            request(app)
-            .patch('/api/v2/auth/reset/1')
-            .send(changePassword).end((err,res) =>{
-                expect(res.status).to.equal(SUCCESS_CODE);
-                expect(res.body).to.have.property('status').equal(SUCCESS_CODE);
-                expect(res.body.data).to.be.an('object');
-                expect(res.body.message).to.be.a("string")
-                done();
-            });
-        });
-
-        it('Should not reset password if it does not match old password',(done)=>{
-            changePassword.old_password = "1234567";
-            request(app)
-            .patch('/api/v2/auth/reset/1')
-            .send(changePassword).end((err,res) =>{
-                expect(res.status).to.equal(UNAUTHORIZED_CODE);
-                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-                expect(res.body.error).to.be.equal(OLD_PASSWORD_NOT_MATCH);
-                done();
-            });
-        });
-        it('Should return 401 if confirm password does not match new password',(done)=>{
-            changePassword.old_password = "123456";
-            changePassword.confirm_password = "1234567b";
-            request(app)
-            .patch('/api/v2/auth/reset/1')
-            .send(changePassword).end((err,res) =>{
-                expect(res.status).to.equal(UNAUTHORIZED_CODE);
-                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-                expect(res.body.error).to.be.equal(PASSWORD_DOESNT_MATCH);
-                done();
-            });
-        });
-        it('Should not reset password if ID is not found',(done)=>{
-            userTable.map(user => { user.id = 2 })
-            request(app)
-            .patch('/api/v2/auth/reset/1')
-            .send(changePassword).end((err,res) =>{
-                expect(res.status).to.equal(UNAUTHORIZED_CODE);
-                expect(res.body).to.have.property('status').equal(UNAUTHORIZED_CODE);
-                expect(res.body.error).to.be.equal(USER_ID_NOT_FOUND);
-                done();
-            });
-        });
-        it('Should not reset password If new password is less than 6',(done)=>{
-            userTable.map(user => { user.id = 1 });
-            changePassword.new_password = "123";
-            request(app)
-            .patch('/api/v2/auth/reset/1')
-            .send(changePassword).end((err,res) =>{
-                expect(res.status).to.equal(UNPROCESSABLE_ENTITY);
-                expect(res.body).to.have.property('status').equal(UNPROCESSABLE_ENTITY);
-                done();
-            });
-        });
-    });
-
-    describe('Admin can view all users',()=>{
-        it('Should return 404. Users not found',(done) => {
-            request(app)
-            .get('/api/v2/users')
-            .set('Authorization', adminTokenId)
-            .end((err,res) => {
-                expect(res.status).to.be.equal(NOT_FOUND_CODE)
-                done();
-            })
-        });
-        it('Should return 200. Find all users',(done) => {
-            userTable.map((user) => { user.is_admin = false})
-            request(app)
-            .get('/api/v2/users')
-            .set('Authorization', adminTokenId)
-            .end((err,res) => {
-                expect(res.status).to.be.equal(SUCCESS_CODE);
-                expect(res.body.data).to.be.an('array');
-                expect(res.body.message).to.be.a("string")
-                done();
-            })
-        });
-    });
 });
-
