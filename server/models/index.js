0c4bb5f303cc065b806504ecf4b16d372eb8d8a8
Eliezer <eliezer.basubi30@gmail.com>
2019-08-13 09:28:46 +0200

    ch(db-setup): Refactor code and improve

--- server/models/index.js
+++ server/models/index.js
@@ -4,9 +4,9 @@ import DB_URL from '../config/config';
 const pool = new Pool({ connectionString: DB_URL });
 
 const dropIntest = {
-  dropUserTable: async () => {
+  dropUserTable: async (sql) => {
     try {
-      const res = await pool.query('DROP TABLE IF EXISTS users');
+      const res = await pool.query(sql);
       return res;
     } catch (e) {
       return {
@@ -19,16 +19,14 @@ const dropIntest = {
 
 
 const creator = {
-  usersTable: async () => {
-    const query = 'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(250) NOT NULL, lastname VARCHAR(250), email VARCHAR(250) NOT NULL UNIQUE,password VARCHAR(250) NOT NULL, phone VARCHAR(15) NOT NULL, country VARCHAR(50), city VARCHAR(50) NOT NULL, isAdmin BOOLEAN DEFAULT false);';
-
+  createTable: async (query) => {
     try {
       const res = await pool.query(query);
       return res;
     } catch (e) {
       return {
         error: true,
-        res: 'Users table not created'
+        res: 'Unable To Create Table'
       };
     }
   }
