c7990ab1dd220be85d3bd02f2d153c7cdc9df81b
Eliezer <eliezer.basubi30@gmail.com>
2019-08-13 15:19:22 +0200

    ft(db-signup): Delivers clean signup endpoint
    - Remove unused files
    - Remove commented out codes
    - Remove create tables in server
    
    [#167828142]

--- server/models/user.js
+++ server/models/user.js
@@ -1,29 +1,25 @@
-export const userTable = [];
-export const cache = [];
-export const userCredentials = [];
+import { pool, creator } from './index';
+import { RESOURCE_CONFLICT } from '../constants/responseCodes';
+import { EMAIL_ALREADY_EXIST } from '../constants/feedback';
 
-export class User {
-  constructor({
-    id,
-    token,
-    first_name,
-    last_name,
-    email,
-    password,
-    phone_number,
-    country,
-    city,
-    is_admin
-  }) {
-    this.token = token;
-    this.id = id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.email = email;
-    this.password = password;
-    this.phone_number = phone_number;
-    this.country = country;
-    this.city = city;
-    this.is_admin = is_admin;
+export const currentUser = [];
+
+export default class UserQuery {
+  static async insert(values) {
+    try {
+      await creator.createTable('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(250) NOT NULL, lastname VARCHAR(250), email VARCHAR(250) NOT NULL UNIQUE,password VARCHAR(250) NOT NULL, phone VARCHAR(15) NOT NULL, country VARCHAR(50), city VARCHAR(50) NOT NULL, isAdmin BOOLEAN DEFAULT false);');
+
+      const exist = await pool.query('SELECT * FROM users WHERE email = $1', [values[2]]);
+      if (exist.rowCount > 0) {
+        return { error: { status: RESOURCE_CONFLICT, message: EMAIL_ALREADY_EXIST } };
+      }
+
+      const result = await pool.query(`INSERT INTO users (firstname, lastname, email,password,phone,country,city
+          ) VALUES($1, $2, $3, $4, $5,$6,$7) RETURNING id, email,isadmin`, values);
+
+      return result;
+    } catch (e) {
+      return { error: { status: 500, message: e, err: e } };
+    }
   }
 }
