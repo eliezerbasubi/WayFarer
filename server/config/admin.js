9ac37bd4546f52bddf4105fc431a9dfe130618aa
Eliezer <eliezer.basubi30@gmail.com>
2019-08-16 05:21:05 +0200

    add super admin in heroku

--- server/config/admin.js
+++ server/config/admin.js
@@ -8,7 +8,6 @@ const superAdmin = async () => {
   const res = await pool.query(`INSERT INTO users (firstname, lastname, email,password,phone,country,city, isadmin) 
   VALUES('${data.firstname}', '${data.lastname}', '${data.email}', '${data.password}',
     '${data.phone}','${data.country}','${data.city}', '${data.isadmin}') on conflict (email) do nothing returning *`);
-  console.log('super admin creating...');
   await pool.end();
   return res;
 };
